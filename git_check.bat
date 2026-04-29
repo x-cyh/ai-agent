4f15a6e5bd3d783db77a3b60c0e813d9703b6aa4
x-cyh <0604yuheng@gmail.com>
2026-04-29 16:49:04 +0800

    update

---
+++ git_check.bat
@@ -0,0 +1,3 @@
+git status --short > status.txt
+git diff --cached > diff_cached.txt
+git diff > diff_unstaged.txt
--- main.py
+++ main.py
@@ -6,17 +6,29 @@ load_dotenv()
 
 def main():
     api_key = os.getenv("OPENAI_API_KEY")
-    agent_name = "Agent 666"
-    # message = f"hi i am {}"
-    if api_key is None:
-        print("未設定 API Key;請檢查 .env 或系統環境變數")
-        exit()
-    else:
-        print(f"API Key found;已設定:{api_key}")
-    
-    # response = llm.invoke("Hi, I'm Agent 666")
-    # print(response.content)
+    agent_name = "Gemma4 Agent"
+    base_url = os.getenv("BASE_URL")
+    model_name = os.getenv("MODEL_NAME")
 
+    if api_key:
+        print(f"API Key found;已設定")
+    else:
+        print("未設定 API Key;請檢查 .env 或系統環境變數")
+        return
+    
+    llm = ChatOpenAI(
+        api_key=api_key,
+        base_url=base_url,
+        model=model_name,
+        temperature=1.0
+    )
+    
+    message = f"你好(用繁體中文回答)"
+    print(f"\n使用者:{message}")
+    
+    response = llm.invoke(message)
+    print(f"\nGemma4 Agent:{response.content}")
+    
 
 
 if __name__ == "__main__":
