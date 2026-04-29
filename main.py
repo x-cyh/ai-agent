f909039d682c4f9e4236802c0125eb027e0fb4cb
x-cyh <0604yuheng@gmail.com>
2026-04-29 16:48:57 +0800

    update

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ load_dotenv()
 
 def main():
     api_key = os.getenv("OPENAI_API_KEY")
-    print(api_key)
-    agent_name = "666"
-    message = f"Hi, I'm {agent_name}"
-    print(message)
-
+    agent_name = "Agent 666"
+    # message = f"hi i am {}"
+    if api_key is None:
+        print("未設定 API Key;請檢查 .env 或系統環境變數")
+        exit()
+    else:
+        print(f"API Key found;已設定:{api_key}")
     
+    # response = llm.invoke("Hi, I'm Agent 666")
+    # print(response.content)
+
 
 
 if __name__ == "__main__":
